de8bef80bdb31531d178b755d962436ed213d7c7
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-07-19 12:41:58 -0300

    Created other entities and made the second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.Linq;
 
 namespace SalesWebMvc.Models
 {
@@ -7,5 +7,28 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+
     }
 }
