44b6a27941b4b5f78ae13632c3c42c516528c4a4
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-07-24 17:19:17 -0300

    Created SellerFormView and Department selct component

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddScoped<SeedingService>();
 
 builder.Services.AddScoped<SellerService>();
 
+builder.Services.AddScoped<DepartmentService>();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,7 +17,7 @@ namespace SalesWebMvc.Services
         }
 
         public void Insert(Seller obj)
-        {            
+        {                  
             _context.Add(obj);
             _context.SaveChanges();
         }
